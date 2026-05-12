6629bc64027f08b712e54fe7128c2c48c33f0a58
Dnyftech <dev@dnyf.platform.com>
2026-05-12 17:23:18 +0100

    Initial platform architecture

---
+++ js/state.js
@@ -0,0 +1,7 @@
+// state.js
+export const AppState = {
+  theme: "dark",
+  currentPage: "home",
+  chats: [],
+  terminalHistory: [],
+};
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icons/*.png"],
+      manifest: {
+        name: "DNYF",
+        short_name: "DNYF",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
